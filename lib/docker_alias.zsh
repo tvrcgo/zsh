4cee1b5f2b9cb26d6684b2296dc15da2a1512869
tvrcgo <tvrcgo@gmail.com>
2018-12-16 23:31:35 +0800

    docker alias

---
+++ lib/docker_alias.zsh
@@ -0,0 +1,58 @@
+alias dkl="dk logs"
+
+dk() {
+ docker $@
+}
+
+dls() {
+ print $fg[yellow]
+ docker images $@
+ print $fg[green]
+ docker ps $@
+}
+
+dla() {
+ dls -a
+}
+
+dup() {
+ docker run -dit --name $1 ${2:-$1}
+}
+
+dbash() {
+ docker exec -it $(docker ps -aqf "name=$1") bash
+}
+
+dsh() {
+ docker exec -it $(docker ps -aqf "name=$1") sh
+}
+
+drm() {
+ if [[ -n $1 ]];then
+  docker stop $(docker ps -a | grep "$1" | awk '{ print $1 }')
+  docker rm $(docker ps -a | grep "$1" | awk '{ print $1 }')
+ fi
+}
+
+drmi() {
+ if [[ -n $1 ]];then
+  docker rmi $(docker images | grep "$1" | awk '{ print $1 ":" $2 }')
+ fi
+}
+
+drmx() {
+ docker rm $(docker ps -qf status=exited)
+ docker rmi $(docker images -qf dangling=true)
+}
+
+dc() {
+ docker-compose $@
+}
+
+dcb() {
+ dc build --no-cache $@
+}
+
+dcu() {
+ dc up -d --force-recreate $@
+}
