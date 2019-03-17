bdf9fa143b120eea1446d7a653e3499cebc454e5
tvrcgo <tvrcgo@gmail.com>
2019-03-18 02:01:56 +0800

    git update

--- lib/git_alias.zsh
+++ lib/git_alias.zsh
@@ -1,16 +1,22 @@
+
 alias gitrc="vi ~/.alias/git_alias.zsh"
 alias g="git"
 alias gi="git status"
 alias gps="git push"
 alias gpl="git pull -r"
 alias gm="git merge --no-ff"
+alias gmctn="git merge --continue"
+alias gmabt="git merge --abort"
 alias gl="git log --color --graph --abbrev-commit --decorate --pretty=format:'%C(magenta)%h%Creset %C(auto)%d%Creset %C(white)%s%Creset %C(cyan)%cn,%Creset %C(cyan)%cr%Creset' --all"
 alias gcl="git clone"
 alias gcm="git commit -m"
-alias gcu="git commit --amend -m"
-alias gca="git commit --amend --no-edit"
+alias gau="git commit --amend -m"
+alias gam="git commit --amend --no-edit"
 alias gfh="git fetch origin --prune"
 alias gdf="git show --color --pretty=format:%b"
+alias gspu="git stash"
+alias gspo="git stash pop"
+
 
 current_branch() {
   git rev-parse --abbrev-ref HEAD
@@ -24,6 +30,27 @@ remote_branch() {
   gb | grep remotes | awk -F remotes/origin/ '{ print $2 }'
 }
 
+gmg() {
+  if [ -z "$1" ]; then
+    echo "Which branch to merge ?"
+    return
+  fi
+
+  lb=`local_branch | grep $1`
+  if [ ! -z "$lb" ]; then
+    echo "Merge local branch: $lb"
+    echo $lb | xargs git merge --no-ff
+  else
+    rb=`remote_branch | grep $1`
+    if [ ! -z "$rb" ]; then
+      echo "Merge remote branch: $rb"
+      echo $rb | xargs git merge --no-ff
+    else
+      echo "Oops, branch not found"
+    fi
+  fi
+}
+
 gk() {
   if [ -z "$1" ]; then
     echo "Which branch to checkout ?"
@@ -32,17 +59,17 @@ gk() {
 
   lb=`local_branch | grep $1`
   if [ ! -z "$lb" ]; then
-    echo "Checkout local branch: $1"
+    echo "Checkout local branch: $lb"
     echo $lb | xargs git checkout
   else
     git fetch --all --prune
     rb=`remote_branch | grep $1`
     if [ ! -z "$rb" ]; then
-      echo "Checkout remote branch: $1"
+      echo "Checkout remote branch: $rb"
       echo $rb | xargs -I {} git checkout -b {} origin/{}
       git pull -r
     else
-      echo "Oops, branch $1 not found"
+      echo "Oops, branch not found"
     fi
   fi
 }
@@ -97,3 +124,4 @@ gtda() {
   gtd $1
   gtdr $1
 }
+
