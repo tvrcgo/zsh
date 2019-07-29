9424151aca4083859399620983032ee8e8842054
tvrcgo <tvrcgo@gmail.com>
2019-07-29 17:31:11 +0800

    enhance docker

--- lib/docker.zsh
+++ lib/docker.zsh
@@ -1,54 +1,69 @@
 
+query_active_containers_by_name() {
+  docker ps -qf "name=$1"
+}
+
+query_active_containers() {
+  docker ps | grep $1 | awk '{ print $1 }'
+}
+
+query_containers() {
+  docker ps -a | grep $1 | awk '{ print $1 }'
+}
+
+query_images() {
+  docker images -a | grep $1 | awk '{ print $3 }'
+}
+
 dk() {
- docker $@
+  docker $@
 }
 
 dlg() {
- docker logs
+  docker logs $(query_active_containers $1)
 }
 
 dls() {
- print $fg[yellow]
- docker images $@
- print $fg[green]
- docker ps $@
+  print $fg[yellow]
+  docker images $@
+  print $fg[green]
+  docker ps $@
 }
 
 dla() {
- dls -a
+  dls -a
 }
 
 drun() {
- docker run -dit --name $1 ${2:-$1}
+  docker run -dit --name $1 ${2:-$1}
 }
 
 dcmd() {
- docker exec -it $(docker ps -qf "name=$1") ${2:-bash}
+  docker exec -it $(query_active_containers $1) ${2:-bash}
 }
 
 drm() {
- if [[ -n $1 ]];then
-  cons=`docker ps -a | grep "$1" | awk '{ print $1 }'`
-  docker stop $cons
-  docker rm $cons
- fi
+  if [[ -n $1 ]];then
+    docker stop $(query_containers $1)
+    docker rm $(query_containers $1)
+  fi
 }
 
 drmi() {
- if [[ -n $1 ]];then
-  docker rmi $(docker images -a | grep "$1" | awk '{ print $3 }')
- fi
+  if [[ -n $1 ]];then
+    docker rmi $(query_images $1)
+  fi
 }
 
 dc() {
- docker-compose $@
+  docker-compose $@
 }
 
 dcbu() {
- dc build --no-cache $@
+  docker-compose build --no-cache $@
 }
 
 dcup() {
- dc up --build -d --force-recreate $@
+  docker-compose up --build -d --force-recreate $@
 }
 
