5b2a148b7b887ee493f14a126d8317020681b8b2
tvrcgo <tvrcgo@gmail.com>
2020-03-23 16:33:51 +0800

    update docker

--- lib/docker.zsh
+++ lib/docker.zsh
@@ -1,18 +1,24 @@
 
-query_active_containers_by_name() {
-  docker ps -qf "name=$1"
-}
-
 query_active_containers() {
-  docker ps | grep $1 | awk '{ print $1 }'
+  # docker ps | grep $1 | awk '{ print $1 }'
+  docker ps -qf name=$1
 }
 
 query_containers() {
-  docker ps -a | grep $1 | awk '{ print $1 }'
+  # docker ps -a | grep $1 | awk '{ print $1 }'
+  docker ps -aqf name=$1
 }
 
 query_images() {
-  docker images -a | grep $1 | awk '{ print $3 }'
+  # docker images -a | grep $1 | awk '{ print $3 }'
+  docker images -aqf reference=*${1}* -f reference=*${1-*}*/* -f reference=*/*${1-*}*
+}
+
+remove_none_images() {
+  imgs=`docker images --filter "dangling=true" -q --no-trunc`
+  if [[ -n $imgs ]]; then
+    docker rmi $imgs
+  fi
 }
 
 dk() {
@@ -20,25 +26,16 @@ dk() {
 }
 
 dlg() {
-  docker logs $(query_containers $1)
+  docker logs $1
 }
 
 dls() {
   # images
   print $fg[yellow]
-  docker images -f reference=${1-*} -f reference=${1-*/*}
-
+  docker images -af reference=*${1}* -f reference=*${1-*}*/* -f reference=*/*${1-*}*
   # containers
   print $fg[green]
-  docker ps -f name=$1
-
-  # networks
-  print $fg[blue]
-  docker network ls -f name=$1
-}
-
-dla() {
-  dls -a
+  docker ps -af name=$1
 }
 
 drun() {
@@ -46,19 +43,38 @@ drun() {
 }
 
 dcmd() {
-  docker exec -it $(query_active_containers $1) /bin/${2:-bash}
+  docker exec -it $(docker ps -f name=$1 | grep $1 | awk '{ print $1 }') /bin/${2-bash}
 }
 
 drm() {
   if [[ -n $1 ]];then
-    docker stop $(query_containers $1)
-    docker rm $(query_containers $1)
+    # stop running containers
+    run_cons=`query_active_containers $1`
+    if [[ -n $run_cons ]]; then
+      docker stop $run_cons
+    fi
+    # remove containers
+    all_cons=`query_containers $1`
+    if [[ -n $all_cons ]]; then
+      docker rm $all_cons
+    else
+      echo "No container match."
+    fi
+  else
+    echo "Container name is ?"
   fi
 }
 
 drmi() {
   if [[ -n $1 ]];then
-    docker rmi $(query_images $1)
+    imgs=`query_images $1`
+    if [[ -n $imgs ]]; then
+      docker rmi $imgs
+    else
+      echo "No image match."
+    fi
+  else
+    echo "Image name is ?"
   fi
 }
 
