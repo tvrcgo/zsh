482fada28be7874dc18d1ce96f6c9b7385e72e1b
tvrcgo <tvrcgo@gmail.com>
2020-11-10 16:54:13 +0800

    python, git

--- lib/python.zsh
+++ lib/python.zsh
@@ -1,3 +1,7 @@
 
-alias pydep="pipreqs . --force"
 alias unmark="python ~/.zsh/bin/unmark.py"
+alias pydep="pipreqs . --force"
+alias pyget="pip install $1 --user"
+alias pyfrz="pip freeze > requirements.txt"
+
+export PATH=/Users/tvrcgo/Library/Python/2.7/bin:$PATH
